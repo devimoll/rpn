66e427f8bc44983a87d65edd187007d11e5cc837
DEVIMOLL <devimoll@d3d9.sakura.ne.jp>
2022-10-28 21:37:43 +0900

    変数名を整えた。

--- src/rpn.cpp
+++ src/rpn.cpp
@@ -54,55 +54,55 @@ void rpn::input_changed()
 {
     stack.clear();
     stack.shrink_to_fit();
-    QString input_text = input->toPlainText();
-    QString shiki(input_text);
-    shiki.replace("\n", " ");
-    QStringList l = shiki.split(" ");
-    l.removeAll("");
-    qDebug() << base << l;
-    for (const QString s : l) {
-        bool ok;
+    const QString input_text = input->toPlainText();
+    QString exprs(input_text);
+    exprs.replace("\n", " ");
+    QStringList elements = exprs.split(" ");
+    elements.removeAll("");
+    qDebug() << base << elements;
+    for (const QString e : elements) {
+        bool is_number;
         double x;
         if (base == 10) {
-            x = s.toDouble(&ok);
+            x = e.toDouble(&is_number);
         }
         else {
-            x = s.toInt(&ok, base);
+            x = e.toInt(&is_number, base);
         }
 
-        if (ok) {
+        if (is_number) {
             push(x);
         }
         else {
-            if (s == "+") {
+            if (e == "+") {
                 if (!set(2)) return;
                 push(t[1] + t[0]);
             }
-            else if (s == "-") {
+            else if (e == "-") {
                 if (!set(2)) return;
                 push(t[1] - t[0]);
             }
-            else if (s == "*") {
+            else if (e == "*") {
                 if (!set(2)) return;
                 push(t[1] * t[0]);
             }
-            else if (s == "/") {
+            else if (e == "/") {
                 if (!set(2)) return;
                 push(t[1] / t[0]);
             }
-            else if (s == "%" || s == "//") {
+            else if (e == "%" || e == "//") {
                 if (!set(2)) return;
                 push(fmod(t[1], t[0]));
             }
-            else if (s == "sqrt") {
+            else if (e == "sqrt") {
                 if (!set(1)) return;
                 push(qSqrt(t[0]));
             }
-            else if (s == "ln") {
+            else if (e == "ln") {
                 if (!set(1)) return;
                 push(qLn(t[0]));
             }
-            else if (s == "~" || s == "not") {
+            else if (e == "~" || e == "not") {
                 if (!set(1)) return;
                 if (std::floor(t[0]) == t[0]) {
                     push(~(int)t[0]);
@@ -112,83 +112,82 @@ void rpn::input_changed()
                     return;
                 }
             }/*
-            else if (s == "&" || s == "and") {
+            else if (e == "&" || e == "and") {
                 set(2);
                 //push(t[1] ^ t[0]);
             }
-            else if (s == "^" || s == "xor") {
+            else if (e == "^" || e == "xor") {
                 set(2);
             }
-            else if (s == "<<") {
+            else if (e == "<<") {
                 set(2);
             }*/
-            else if (s == "**" || s == "pow") {
+            else if (e == "**" || e == "pow") {
                 if (!set(2)) return;
                 push(qPow(t[1], t[0]));
             }
-            else if (s == "pi") {
+            else if (e == "pi") {
                 push(M_PI);
             }
-            else if (s == "unix") {
+            else if (e == "unix") {
                 push(time(NULL));
             }
-            else if (s == "bin") {
+            else if (e == "bin") {
                 base = 2;
                 input->setText("");
                 return;
             }
-            else if (s == "oct") {
+            else if (e == "oct") {
                 base = 8;
                 input->setText("");
                 return;
             }
             // dec dato 16sinsuuno suuji dakara 16sinsuu kara kirikaerarenai.
-            else if (s == "deci") {
+            else if (e == "deci") {
                 base = 10;
                 input->setText("");
                 return;
             }
-            else if (s == "hex") {
+            else if (e == "hex") {
                 int base_old = base;
-                bool okok;
                 base = 16;
-                QString str("");
+                exprs = "";
                 /* listを1個ずつ読む
                  * 数値なら前のbaseで変数にtointしてqstring::numberで新たなbaseを指定してテキストボックスに追加
                  * 演算子ならそのままテキストボックスに追加
                  * 之で良い気がする*/
-                for (int i = 0; i < l.size() - 1; i++) {
+                for (int i = 0; i < elements.size() - 1; i++) {
                     if (base_old == 10) {
-                        x = l[i].toDouble(&okok);
+                        x = elements[i].toDouble(&is_number);
                     }
                     else {
-                        x = l[i].toInt(&okok, base_old);
+                        x = elements[i].toInt(&is_number, base_old);
                     }
-                    if (okok) {
+                    if (is_number) {
                         if (base == 10) {
-                            str += QString::number(x);
+                            exprs += QString::number(x);
                         }
                         else {
-                            str += QString::number((int)x, base);
+                            exprs += QString::number((int)x, base);
                         }
                     }
                     else {
-                        str += l[i];
+                        exprs += elements[i];
                     }
-                    str += " ";
+                    exprs += " ";
                 }
 
-                input->setText(str);
+                input->setText(exprs);
                 return;
             }
-            else if (s == "help" || s == "hh") {
-                input_text.remove("help");
-                input_text.remove("hh");
+            else if (e == "help" || e == "hh") {
+                exprs.remove("help");
+                exprs.remove("hh");
                 qDebug() << "help";
                 QTextCursor tc = input->textCursor();
                 // よくわからないが1を引かないとエラーが出る
                 tc.setPosition(input->document()->characterCount() - 1);
-                input->setText(input_text);
+                input->setText(exprs);
                 // この行はinput->setText(input_text);より下に無いとinvaliedで上書きされる
                 result->setText("+, -, *, /, sqrt, ln");
                 input->setTextCursor(tc);
