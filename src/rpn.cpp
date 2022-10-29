f46519c1d992bb7d4ab02928bff0c9cdf3a0bebc
DEVIMOLL <devimoll@d3d9.sakura.ne.jp>
2022-10-29 10:32:43 +0900

    help拡充とhypot追加

--- src/rpn.cpp
+++ src/rpn.cpp
@@ -14,6 +14,14 @@ rpn::rpn(QWidget *parent)
     setLayout(layout);
     connect(input, &QTextEdit::textChanged,
             this, &rpn::input_changed);
+    QString help = "+ - * / %/// acos acosh asin asinh atan2 atan atanh\
+ ceil cos cosh dtr exp abs floor ln log lb **/pow rtd sin sinh sqrt tan\
+ tanh gamma ~/not &/and |/or ^/xor << >> pi e unix bin oct deci hex help/hh";
+    help_dialog.setParent(this);
+    help_dialog.setWindowTitle("rpn - help");
+    help_dialog.setText(help);
+    help_dialog.setStandardButtons(QMessageBox::Close);
+    help_dialog.setWindowModality(Qt::NonModal);
 }
 
 rpn::~rpn()
@@ -25,7 +33,8 @@ rpn::~rpn()
 
 double rpn::pop() {
     if (stack.isEmpty()) {
-        qDebug() << "stack is empty but tried pop().";
+        qDebug() << "Quamquam acervus vacuus erat, pop() appellatus est.";
+        QMessageBox::critical(this, "EXITIALE ERROR", "Quamquam acervus vacuus erat, pop() appellatus est.");
         exit(1);
     }
     double x = stack.back();
@@ -48,6 +57,11 @@ bool rpn::set(int n) {
         t.append(pop());
     }
     return true;
+}
+
+bool rpn::is_integer(double n)
+{
+    return std::floor(n) == n;
 }
 
 // カーソル位置を保持したままテキストボックスに書き込む
@@ -97,7 +111,7 @@ void rpn::input_changed()
     exprs.replace("\n", " ");
     QStringList elements = exprs.split(" ");
     elements.removeAll("");
-    qDebug() << base << elements;
+    //qDebug() << base << elements;
     for (const QString e : elements) {
         bool is_number;
         double x;
@@ -132,40 +146,180 @@ void rpn::input_changed()
                 if (!set(2)) return;
                 push(fmod(t[1], t[0]));
             }
-            else if (e == "sqrt") {
+            else if (e == "acos") {
                 if (!set(1)) return;
-                push(qSqrt(t[0]));
+                push(qAcos(t[0]));
+            }
+            else if (e == "acosh") {
+                if (!set(1)) return;
+                push(std::acosh(t[0]));
+            }
+            else if (e == "asin") {
+                if (!set(1)) return;
+                push(qAsin(t[0]));
+            }
+            else if (e == "asinh") {
+                if (!set(1)) return;
+                push(std::asinh(t[0]));
+            }
+            else if (e == "atan2") {
+                if (!set(2)) return;
+                push(qAtan2(t[1], t[0]));
+            }
+            else if (e == "atan") {
+                if (!set(1)) return;
+                push(qAtan(t[0]));
+            }
+            else if (e == "atanh") {
+                if (!set(1)) return;
+                push(std::atanh(t[0]));
+            }
+            else if (e == "ceil") {
+                if (!set(1)) return;
+                push(qCeil(t[0]));
+            }
+            else if (e == "cos") {
+                if (!set(1)) return;
+                push(qCos(t[0]));
+            }
+            else if (e == "cosh") {
+                if (!set(1)) return;
+                push(std::cosh(t[0]));
+            }
+            else if (e == "dtr") {
+                if (!set(1)) return;
+                push(qDegreesToRadians(t[0]));
+            }
+            else if (e == "exp") {
+                if (!set(1)) return;
+                push(qExp(t[0]));
+            }
+            else if (e == "abs") {
+                if (!set(1)) return;
+                push(qFabs(t[0]));
+            }
+            else if (e == "floor") {
+                if (!set(1)) return;
+                push(qFloor(t[0]));
+            }
+            else if (e == "hypot") {
+                if (!set(2)) return;
+                push(std::hypot(t[1], t[0]));
             }
             else if (e == "ln") {
                 if (!set(1)) return;
                 push(qLn(t[0]));
             }
-            else if (e == "~" || e == "not") {
+            else if (e == "log") {
                 if (!set(1)) return;
-                if (std::floor(t[0]) == t[0]) {
-                    push(~(int)t[0]);
-                }
-                else {
-                    result->setText("The operand of ~ is not integer.");
-                    return;
-                }
-            }/*
-            else if (e == "&" || e == "and") {
-                set(2);
-                //push(t[1] ^ t[0]);
+                push(std::log10(t[0]));
             }
-            else if (e == "^" || e == "xor") {
-                set(2);
+            else if (e == "lb") {
+                if (!set(1)) return;
+                push(std::log2(t[0]));
             }
-            else if (e == "<<") {
-                set(2);
-            }*/
             else if (e == "**" || e == "pow") {
                 if (!set(2)) return;
                 push(qPow(t[1], t[0]));
             }
+            else if (e == "rtd") {
+                if (!set(1)) return;
+                push(qRadiansToDegrees(t[0]));
+            }
+            else if (e == "sin") {
+                if (!set(1)) return;
+                push(qSin(t[0]));
+            }
+            else if (e == "sinh") {
+                if (!set(1)) return;
+                push(std::sinh(t[0]));
+            }
+            else if (e == "sqrt") {
+                if (!set(1)) return;
+                push(qSqrt(t[0]));
+            }
+            else if (e == "tan") {
+                if (!set(1)) return;
+                push(qTan(t[0]));
+            }
+            else if (e == "tanh") {
+                if (!set(1)) return;
+                push(std::tanh(t[0]));
+            }
+            else if (e == "gamma") {
+                if (!set(1)) return;
+                push(std::tgamma(t[0]));
+            }
+            else if (e == "~" || e == "not") {
+                if (!set(1)) return;
+                if (is_integer(t[0])) {
+                    push(~(int)t[0]);
+                }
+                else {
+                    result->setText("The operand of ~/not is not integer.");
+                    return;
+                }
+            }
+            else if (e == "&" || e == "and") {
+                if (!set(2)) return;
+                if (is_integer(t[0])
+                 && is_integer(t[1])) {
+                    push((int)t[1] & (int)t[0]);
+                }
+                else {
+                    result->setText("The operand of &/and is not integer.");
+                    return;
+                }
+            }
+            else if (e == "|" || e == "or") {
+                if (!set(2)) return;
+                if (is_integer(t[0])
+                 && is_integer(t[1])) {
+                    push((int)t[1] | (int)t[0]);
+                }
+                else {
+                    result->setText("The operand of |/or is not integer.");
+                    return;
+                }
+            }
+            else if (e == "^" || e == "xor") {
+                if (!set(2)) return;
+                if (is_integer(t[0])
+                 && is_integer(t[1])) {
+                    push((int)t[1] ^ (int)t[0]);
+                }
+                else {
+                    result->setText("The operand of ^/xor is not integer.");
+                    return;
+                }
+            }
+            else if (e == "<<") {
+                if (!set(2)) return;
+                if (is_integer(t[0])
+                 && is_integer(t[1])) {
+                    push((int)t[1] << (int)t[0]);
+                }
+                else {
+                    result->setText("The operand of << is not integer.");
+                    return;
+                }
+            }
+            else if (e == ">>") {
+                if (!set(2)) return;
+                if (is_integer(t[0])
+                 && is_integer(t[1])) {
+                    push((int)t[1] >> (int)t[0]);
+                }
+                else {
+                    result->setText("The operand of >> is not integer.");
+                    return;
+                }
+            }
             else if (e == "pi") {
                 push(M_PI);
+            }
+            else if (e == "e" && base == 10) {
+                push(M_E);
             }
             else if (e == "unix") {
                 push(time(NULL));
@@ -198,15 +352,28 @@ void rpn::input_changed()
             else if (e == "help" || e == "hh") {
                 exprs.remove("help");
                 exprs.remove("hh");
-                QString help = "+, -, *, /, sqrt, ln";
                 set_input_box_text(exprs);
-                // この行はset_input_box_text()内のinput->setText(input_text);より下に無いとinvaliedで上書きされる
-                result->setText(help);
+                // https://doc.qt.io/qt-6/application-windows.html#window-geometry
+                help_dialog.setGeometry(
+                    this->geometry().x(),
+                    this->geometry().y() + this->frameGeometry().height(),
+                    1,  // 無視されるからなんでもよい
+                    1   // 無視されるからなんでもよい
+                );
+                // exec()だとモーダルウヰンドウになってしまう
+                help_dialog.show();
                 // このreturnがないとinvalidで上書きされる
                 return;
             }
+            else if (e == "aboutqt") {
+                exprs.remove("aboutqt");
+                set_input_box_text(exprs);
+                QMessageBox msgbox;
+                msgbox.aboutQt(this, "About Qt");
+                return;
+            }
             else {
-                result->setText("Invalid Operator");
+                result->setText("Invalid Operator: " + e);
                 return;
             }
         }
@@ -219,10 +386,16 @@ void rpn::input_changed()
         return;
     }
     if (base == 10) {
-        result->setText(QString::number(stack.back()));
+        result->setText("0d " + QString::number(stack.back()));
+    }
+    else if (base == 2) {
+        result->setText("0b " + QString::number((int)stack.back(), base));
+    }
+    else if (base == 8) {
+        result->setText("0o " + QString::number((int)stack.back(), base));
     }
     else {
-        result->setText(QString::number((int)stack.back(), base));
+        result->setText("0x " + QString::number((int)stack.back(), base));
     }
 }
 
--- src/rpn.h
+++ src/rpn.h
@@ -26,12 +26,15 @@ private:
     QVector<double> stack;
     QVector<double> t; // term
     int base;
+    QMessageBox help_dialog;
 
     double pop();
     void push(double);
     bool set(int);
+    bool is_integer(double);
     void set_input_box_text(QString);
     void convert_base(int, int, const QStringList&);
+
     void input_changed();
 };
 #endif // RPN_H
